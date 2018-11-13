e8fb06c2cd93f052553b759c997c0042497cd89f
albot <alboteanud@gmail.com>
2018-11-13 13:41:35 +0200

    added SW

--- js/main.js
+++ js/main.js
@@ -1,87 +1,68 @@
 const USERS = ['makeschool', 'newsycombinator', 'ycombinator'];
+const NUMBER_OF_REQUESTED_TWEETS = 6;
 const KEY_COLUMN = 'column_';
 const TARGET_ID = 'target_id';
 const PROXY_URL = 'http://localhost:7890';
-const NUMBER_OF_REQUESTED_TWEETS = 6;
+const USER_TIMELIN_URL = `${PROXY_URL}/1.1/statuses/user_timeline.json\?count\=${NUMBER_OF_REQUESTED_TWEETS}\&screen_name\=`;
 
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    for (let i = 0; i < USERS.length; i++) {
-        // const user = localStorage.getItem(KEY_COLUMN + i) || USERS[i];
-        const user =  USERS[i];
-        const list = initTweetsListHTML(user, null);
-        fetchTweets(user, list, (json) => {
-            // console.log(json);
-            initTweetsListHTML(user, json,i);
-        });
-    }
-    
-    // fetchTweets(USERS, (userTweets) => {
-    //     initTweetsListHTML(userTweets);
-    // });
-    
+    USERS.forEach(user => fetchTweets(user, (user, tweets) => setTweetsList(user, tweets)))
 });
 
-function fetchTweets(user_scr_name, list, callback) {
-    
-    
-    const url = `${PROXY_URL}/1.1/statuses/user_timeline.json\?count\=${NUMBER_OF_REQUESTED_TWEETS}\&screen_name\=${user_scr_name}`;
+function fetchTweets(user, callback) {
+    const url = USER_TIMELIN_URL+ user;
+    // console.log('getting tweets from ' + url);
     fetch(url).then(response => {
         if (response.status !== 200) {
             console.log(`Looks like there was a problem fetching the tweets. Response status Code: ${response.status}`);
             return;
         }
-        response.json().then(tweetsJson => {
-            callback(tweetsJson);
-            tweetsJson.forEach(tweetJson => {
-                const tweetCard = createTweetHTML(tweetJson);
-                list.append(tweetCard);
-                // TODO add tweet to IDB for offline use
-            });
-        })
-        
+        response.json().then(tweetsJson => callback(user, tweetsJson));
     });
 }
 
-function initTweetsListHTML(user_scr_name,  json, i) {
-    if(json!=null){
-        console.log(json[0].user);
-        document.getElementById(user_scr_name).querySelector('.profile-img').src = json[0].user.profile_image_url
-       
-        const loc = localStorage.getItem(KEY_COLUMN + i) ;
-        //  document.getElementById(user_scr_name).setAttribute('grid-item:nth-child', loc)
-        document.getElementById(user_scr_name).setAttribute('grid-column', loc)
-        document.getElementById(user_scr_name).setAttribute('grid-row', loc)
-        return;
-    }
-//grid-item:nth-child
+function setTweetsList(user, tweets) {
     const temp = document.getElementById('column_template');
     const clon = temp.content.cloneNode(true);
-    
-    const div = clon.querySelector('div');
-    div.id = user_scr_name;
-    div.querySelector('.profile-name').innerHTML = Utils.capitalizeFirstLetter(user_scr_name);
-    
-    document.getElementById('container').appendChild(div);
-    return div.querySelector('ul');
+    const column = clon.querySelector('div');
+
+    column.id = user;
+    document.getElementById('container').appendChild(column);
+
+    const orderClassName = localStorage.getItem(user) || 
+        `column${USERS.indexOf(user) + 1}`; // default class to order columns: column1, column2, column3
+    column.classList.add(orderClassName);
+
+    column.querySelector('.profile-img').src = tweets[0].user.profile_image_url;
+    column.querySelector('.profile-name').innerHTML = Utils.capitalizeFirstLetter(user);
+
+    const list = column.querySelector('ul');
+    tweets.forEach(tweet => {
+        const tweetCard = createTweetHTML(tweet);
+        list.append(tweetCard);
+        // TODO add tweet to IDB for offline use
+    });
+
 }
 
 function createTweetHTML(tweetJson) {
-    
+
     const temp = document.getElementById('tweet_card_template');
     const clon = temp.content.cloneNode(true);
     const li = clon.querySelector('li');
-    
+
     li.querySelector('.tweet_text').innerHTML = Utils.pimpText(tweetJson.text);
     li.querySelector('.tweet_date').innerHTML = Utils.getFormatedDate(tweetJson.created_at);
     li.querySelector('.tweet_link').href = `https://twitter.com/${tweetJson.user.id_str}/status/${tweetJson.id_str}`;
     li.querySelector('.tweet_retweet').href = `https://twitter.com/intent/retweet?tweet_id=${tweetJson.id_str}&related=${tweetJson.user.screen_name}`;
-    
+
     return li;
 }
 
 function handleDragStart(ev) {
     ev.dataTransfer.setData(TARGET_ID, ev.target.id);
+    console.log(ev.target.id);
 }
 
 function handleDragOver(ev) {
@@ -90,25 +71,19 @@ function handleDragOver(ev) {
 
 function handleDrop(ev) {
     ev.preventDefault();
-    const drag_list_id = ev.dataTransfer.getData(TARGET_ID);
-    const drag_list = document.getElementById(drag_list_id);
-    
-    const tmp = document.createElement('span');
-    tmp.className = 'hide';
-    
-    ev.currentTarget.before(tmp);
-    drag_list.before(ev.currentTarget);
-    tmp.replaceWith(drag_list);
-    
-    saveColumnsOrder();
-}
+    const dragged_id = ev.dataTransfer.getData(TARGET_ID);
+    const dragged_column = document.getElementById(dragged_id);
 
-function saveColumnsOrder() {
-    const column_0_id = document.getElementsByClassName('column')[0].id;
-    const column_1_id = document.getElementsByClassName('column')[1].id;
-    const column_2_id = document.getElementsByClassName('column')[2].id;
-    
-    localStorage.setItem(KEY_COLUMN + 0, column_0_id);
-    localStorage.setItem(KEY_COLUMN + 1, column_1_id);
-    localStorage.setItem(KEY_COLUMN + 2, column_2_id);
+    const dragged_class = dragged_column.classList[1];
+    const target_class = ev.currentTarget.classList[1];
+
+    dragged_column.classList.remove(dragged_class);
+    dragged_column.classList.add(target_class);
+
+    ev.currentTarget.classList.remove(target_class);
+    ev.currentTarget.classList.add(dragged_class);
+
+    localStorage.setItem(dragged_id, target_class);
+    localStorage.setItem(ev.currentTarget.id, dragged_class);
+
 }
